df0408d776365ca5fce391eef6e5ec7d3f8e9933
pnku <pnku@pnku.de>
2026-03-30 23:29:52 +0200

    Remove unused code

--- src/main/java/de/pnku/mbhv/block/MoreBeehiveVariantBlock.java
+++ src/main/java/de/pnku/mbhv/block/MoreBeehiveVariantBlock.java
@@ -1,7 +1,5 @@
 package de.pnku.mbhv.block;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.SoundType;
@@ -18,42 +16,5 @@ public class MoreBeehiveVariantBlock extends BeehiveBlock {
     public MoreBeehiveVariantBlock(MapColor colour, SoundType sound, String beehiveWoodType) {
         super(Properties.ofFullCopy(Blocks.BEEHIVE).mapColor(colour).sound(sound));
         this.beehiveWoodType = beehiveWoodType;
-    }
-
-    public Item getPlanksItem(String planksWood) {
-        switch (planksWood) {
-            case "acacia" -> {
-                return Items.ACACIA_PLANKS;
-            }
-            case "bamboo" -> {
-                return Items.BAMBOO_PLANKS;
-            }
-            case "birch" -> {
-                return Items.BIRCH_PLANKS;
-            }
-            case "cherry" -> {
-                return Items.CHERRY_PLANKS;
-            }
-            case "crimson" -> {
-                return Items.CRIMSON_PLANKS;
-            }
-            case "dark_oak" -> {
-                return Items.DARK_OAK_PLANKS;
-            }
-            case "jungle" -> {
-                return Items.JUNGLE_PLANKS;
-            }
-            case "mangrove" -> {
-                return Items.MANGROVE_PLANKS;
-            }
-            case "spruce" -> {
-                return Items.SPRUCE_PLANKS;
-            }
-            case "warped" -> {
-                return Items.WARPED_PLANKS;
-            }
-
-        }
-        return null;
     }
 }
